7ff78c7e7441f718ed2ce713fe97f114d5b01a20
cuijiali <435862941@qq.com>
2017-10-19 13:11:38 +0800

    pmd

--- pkg/lab1.java
+++ pkg/lab1.java
@@ -1,14 +1,88 @@
-package src.pkg;
-// pmd 的提示就不改了;
+package pkg;
+//pmd 要改的太多了，不改了
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
-import java.util.Scanner;
 import java.util.Vector;
-import src.pkg.Graph;
-
+import java.util.Scanner;
+class Graph{
+    private Vector<String> nodes = new Vector<String>();
+    private Vector<String> source = new Vector<String>();
+    private boolean isGraph;
+    public int matrix[][], bridge[][][];
+    private boolean vis[];
+    Graph() {
+        this.nodes = new Vector<String>();
+        this.source = new Vector<String>();
+    }
+    Graph createDirectedGraph(String fileName) {
+        try {
+            nodes.clear(); source.clear();
+            FileReader fr = new FileReader(new File(fileName));
+            int temp = 0;
+            StringBuilder tempWord = new StringBuilder();
+            while((temp = fr.read())!= -1) {
+                if (('a'<=temp&&temp<='z')||('A'<=temp&&temp<='Z')){
+                    tempWord.append((char)temp);
+                }
+                else if (temp != ',' && temp != '.' && temp != ';' && temp != ':' && temp != '\''
+                        && temp != '"' && temp != '-' && temp != '(' && temp != ')' && temp != '!'
+                        && temp != '?' && temp != ' ' && temp != '\n')
+                    continue;
+                else {
+                    String addWord = tempWord.toString().toLowerCase();
+                    if(addWord.length() != 0) {
+                        this.source.addElement(addWord);
+                        if (this.findNode(addWord) == -1)
+                            this.nodes.addElement(addWord);
+                    }
+                    tempWord.delete(0, tempWord.length());
+                }
+            }
+            fr.close();
+        } catch(IOException e) {
+            System.out.println("The path you input is wrong, please input again after checking");
+            isGraph = false;
+            return this;
+        }
+        int numb = this.nodes.size();
+        matrix = new int [numb][numb];
+        int preIndex=this.findNode(this.source.elementAt(0)), newIndex;
+        for(int i = 1; i < this.source.size();i++) {
+            newIndex = this.findNode(this.source.elementAt(i));
+            matrix[preIndex][newIndex]++;
+            preIndex = newIndex;
+        }
+        bridge = new int [numb][numb][numb];
+        for(int i = 0; i < numb; i ++){
+            for(int j = 0; j < numb; j ++){
+                for(int k = 0; k < numb; k ++){
+                    if(matrix[i][j]!=0&&matrix[j][k]!=0) {
+                        bridge[i][k][++bridge[i][k][0]]=j;
+                    }
+                }
+            }
+        }
+        isGraph = true;
+        return this;
+    }
+    boolean isGraph(){
+        return  isGraph;
+    }
+    int findNode(String str) {
+        str.toLowerCase();
+        for (int i = 0; i < this.nodes.size(); i++) {
+            if (this.nodes.elementAt(i).equals(str))
+                return i;
+        }
+        return -1;
+    }
+    String getNode(int x) {
+        return this.nodes.elementAt(x);
+    }
+}
 public class lab1{
     public static String fPath;
     public static Integer id = 0;
@@ -125,7 +199,7 @@ public class lab1{
 	private static void showDirectGraph(Graph gra) {
 		String pathName = writeScript(gra, "graph");
 		if(pathName.equals("error!")){
-		    //System.out.println("writeScript error!!!");
+		    System.out.println("writeScript error!!!");
         }
 		try{
 			Thread.sleep(300);
@@ -164,7 +238,7 @@ public class lab1{
 			}
 	}
 	private static String generateNewTest(Graph gra, String inputText) {
-	    inputText = inputText.toLowerCase();
+	    inputText.toLowerCase();
 		String temp[] = inputText.split(" ");
 		Vector<String> text = new Vector<String>();
 		text.addElement(temp[0]);
@@ -337,8 +411,7 @@ public class lab1{
     }
     private static void drawPicture(String pathName){
         try {
-            Runtime.getRuntime().exec
-            ("rundll32 c:\\Windows\\System32\\shimgvw.dll,ImageView_Fullscreen "+pathName);
+            Runtime.getRuntime().exec("rundll32 c:\\Windows\\System32\\shimgvw.dll,ImageView_Fullscreen "+pathName);
         } catch (IOException e) {
             e.printStackTrace();
         }
